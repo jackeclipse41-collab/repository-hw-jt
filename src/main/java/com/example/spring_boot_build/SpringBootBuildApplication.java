af48dec52b1c671e1bf6b17adf036e71245db091
Jesus Isa <jesusisa@Jesuss-MacBook-Air.local>
2026-03-14 17:02:53 -0400

    Added Spring Boot Java 17 project

---
+++ src/main/java/com/example/spring_boot_build/SpringBootBuildApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_build;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_build/SpringBootBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_build;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
